e450c1c63660ee11b53aa960a8325f4ab2efdb45
Luca Bülles <luca.bulles@alten.nl>
2023-09-29 10:16:38 +0200

    added Model patient

---
+++ PatientService/Models/Patient.cs
@@ -0,0 +1,24 @@
+﻿namespace PatientService.Models
+{
+    public class Patient
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Gender { get; set; } = string.Empty;
+        public DateOnly BirthDate { get; set; }
+        public bool ProfileIsActive { get; set; }
+
+        public Patient(int id, string firstName, string lastName, string gender, DateOnly birthDate, bool profileIsActive)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Gender = gender;
+            BirthDate = birthDate;
+            ProfileIsActive = profileIsActive;
+        }
+
+    }
+}
